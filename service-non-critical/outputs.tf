0659712b00ca465028882a95cd2e06d748a69fb2
limed <limed@mozilla.com>
2018-02-14 14:39:34 -0800

    Remove extra module

--- service-non-critical/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "service_id" {
-  value = "${pagerduty_service.service.id}"
-}
-
-output "service_integration_key" {
-  value = "${pagerduty_service_integration.integration.integration_key}"
-}
